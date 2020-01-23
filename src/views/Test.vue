4276e5bf481299c5879ddd49eafc8ab9036fd431
tokoroten-lab <tokoroten.lab@gmail.com>
2020-01-24 01:13:03 +0900

    display sentences on Test page

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,31 +1,50 @@
 <template>
   <div class="test">
     <div v-if="word !== null" class="problem">
-      <h1 class="text-center error--text">{{ word.word }}</h1>
-      <v-progress-linear :value="100 * word.evaluation / word.appear_count" class="mb-5"></v-progress-linear>
-      <h3 class="text-center my-2">Appear count: {{ word.appear_count }}, Correct count: {{ word.correct_count }}, Incorrect count: {{ word.incorrect_count }}</h3>
-      <h3 class="mt-2 mb-4">Evaluation: {{ word.evaluation }}</h3>
-      <div class="text-center">
-        <v-btn
-          outlined
-          color="success"
-          class="mr-2"
-          @click="correct"
-        >
-          I know.
-        </v-btn>
-        <v-btn
-          outlined
-          color="error"
-          @click="incorrect"
-        >
-          I don't know.
-        </v-btn>
-      </div>
+      <v-row class="py-6">
+        <v-col cols="12">
+          <h1 class="text-center error--text">{{ word.word }}</h1>
+          <v-progress-linear :value="100 * word.evaluation / word.appear_count" class="mb-5"></v-progress-linear>
+          <h3 class="text-center my-2">Appear count: {{ word.appear_count }}, Correct count: {{ word.correct_count }}, Incorrect count: {{ word.incorrect_count }}</h3>
+          <h3 class="text-center mt-2 mb-4">Evaluation: {{ word.evaluation }}</h3>
+          <div class="text-center">
+            <v-btn
+              outlined
+              color="success"
+              class="mr-2"
+              @click="correct"
+            >
+              I know.
+            </v-btn>
+            <v-btn
+              outlined
+              color="error"
+              @click="incorrect"
+            >
+              I don't know.
+            </v-btn>
+          </div>
+        </v-col>
+      </v-row>
+
+      <v-divider class="my-4" />
+      <h2 class="text-center py-2">Sentences</h2>
+
+      <v-list>
+        <template v-for="(sentence, index) in sentences">
+          <v-list-item :key="sentence.normal">
+            <v-list-item-content>
+              <v-list-item-subtitle>{{ sentence.original }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider :key="index" />
+        </template>
+      </v-list>
     </div>
     <div v-else>
       <h1>None word.</h1>
     </div>
+
   </div>
 </template>
 
@@ -38,6 +57,7 @@ export default {
 
   data: () => ({
     word: null, 
+    sentences: [],
   }),
 
   methods: {
@@ -61,7 +81,22 @@ export default {
         if(row !== undefined) {
           row.evaluation = row.appear_count * ((row.incorrect_count + 1) / (row.incorrect_count + row.correct_count + 1))
           this.word = row;
+
+          this.loadSentences()
         }
+      });
+    },
+
+    loadSentences() {
+      console.log(this.$store.getters.selectedPond.id)
+      console.log(this.word)
+      db.all('SELECT * FROM word, sentence WHERE word.pond_id = $pond_id AND word.normal = $word AND word.sentence_id = sentence.id GROUP BY sentence.id;', {
+        $pond_id: this.$store.getters.selectedPond.id,
+        $word: this.word.word,
+      },
+      (err, rows) => {
+        console.log(rows)
+        this.sentences = rows;
       });
     },
 
@@ -94,12 +129,3 @@ export default {
   },
 }
 </script>
-
-<style scoped>
-.test {
-  display: flex;
-  height: calc(100vh - 80px);
-  align-items: center;
-  justify-content: center;
-}
-</style>
